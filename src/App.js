9ad7568e69b76c841352157e52b437c04719315e
Sandravijumon <sandravijumon15@gmail.com>
2024-06-07 14:12:24 +0530

    Project created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import AddBook from './components/AddBook';
+import Search from './components/Search';
+import Delete from './components/Delete';
 
 function App() {
   return (
     <div>
       <AddBook/>
+      <Search/>
+      <Delete/>
      
     </div>
   );
