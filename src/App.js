5099dab5f1f6ea9c214d140c8af5a93bf6565183
Sandravijumon <sandravijumon15@gmail.com>
2024-06-07 13:37:29 +0530

    book added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import AddBook from './components/AddBook';
+
+function App() {
+  return (
+    <div>
+      <AddBook/>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/AddBook.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+
+const AddBook = () => {
+  return (
+    <div>
+
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Book title</label>
+                            <input type="text" className="form-control" />
+                        </div>
+
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                            <label htmlFor="" className="form-label">Published Year</label>
+                            <input type="date" name="" id="" className="form-control" />
+
+                        </div>
+
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Description</label>
+                            <textarea name="" id="" className="form-control"></textarea>
+                        </div>
+
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Distributor name</label>
+                            <input type="date" name="" id="" className="form-control" />
+                        </div>
+
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                        <label htmlFor="" className="form-label">Price</label>
+                        <input type="date" name="" id="" className="form-control" />
+
+                        </div>
+
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                        <label htmlFor="" className="form-label">Category</label>
+                        <select name="" id="" className="form-control">
+                            <option className="option">Fiction</option>
+                            <option className="option">Real</option>
+                            <option className="option">Autobiography</option>
+                            <option className="option">History</option>
+                            <option className="option">Novel</option>
+                        </select>
+
+                        </div>
+
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Author name</label>
+                        <input type="date" name="" id="" className="form-control" />
+                        </div>
+
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Publisher name</label>
+                        <input type="date" name="" id="" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Edition</label>
+                        <input type="date" name="" id="" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <button className="btn btn-success">Submit</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AddBook
